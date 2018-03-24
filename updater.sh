27889896f44be3074f3713143af21b44c0443a26
programmerrush <37672619+programmerrush@users.noreply.github.com>
2018-03-24 23:58:03 +0530

    Create updater.sh

---
+++ updater.sh
@@ -0,0 +1,54 @@
+ ____
+|  _ \ _ __ ___   __ _ _ __ __ _ _ __ ___  _ __ ___   ___ _ __
+| |_) | '__/ _ \ / _` | '__/ _` | '_ ` _ \| '_ ` _ \ / _ \ '__|
+|  __/| | | (_) | (_| | | | (_| | | | | | | | | | | |  __/ |
+|_|   |_|  \___/ \__, |_|  \__,_|_| |_| |_|_| |_| |_|\___|_|
+                 |___/      
+ 
+#Author : ProgrammerRush
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/dodo/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/dodo
+
+clear
+
+mkdir dodo
+
+clear
+
+cd dodo
+
+clear
+
+git clone https://github.com/programmerrush/hash1.git
+
+clear
+
+cd dodo
+
+clear
+
+sudo chmod +x installer.sh
+
+clear
+
+./installer.sh
+
+clear
